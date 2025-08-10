20fec3b55bf929a4d46271888bebe4a32096af5f
Rafael <rafael-domingos@hotmail.com>
2025-08-10 17:11:09 -0300

    "test: home - access save room "

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,14 @@
+describe('Home', () => {
+const user_name = Cypress.env('USER_NAME')
+const save_room = Cypress.env('SAVE_ROOM')  
+
+    it('should access save room correctly', () => {
+        cy.visit('/')
+        cy.get('[placeholder="Seu Nickname"]').type(user_name)
+        cy.get('[placeholder="Código da Sala (opcional)"]').type(save_room)
+        cy.contains('Entrar ou Criar Sala').click()
+        cy.contains('RAFFA001 (Admin)').should('be.visible')
+        cy.contains('Famí001').should('be.visible')
+        cy.contains('Sala Salva').should('be.visible')
+    })
+})

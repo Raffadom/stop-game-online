264c71f0a34cc6adaf311f50c3f76d9d3ea52364
Rafael <rafael-domingos@hotmail.com>
2025-11-04 21:07:09 -0300

    "Fix: pipeline corrections"

--- stop-game-frontend/vitest.config.js
+++ stop-game-frontend/vitest.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'node14'
+  },
   test: {
     globals: true,
     environment: 'jsdom',
